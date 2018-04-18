8cb2b0fa0f88c8efd98eaf8d831b554ccb134532
Denis Bystrov <Denis.Bystrov@open.ru>
2018-04-18 16:16:19 +0400

    add geo hfl

---
+++ Geoname.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HFLabsGeonames
+{
+    /// <summary>
+    /// шаблон табл геонеймса
+    /// </summary>
+    class Geoname
+    {
+        /// <summary>
+        /// чтение шаблона
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static List<string> ReadTemplate(string fileName)
+        {
+            char separator = ':';
+            List<string> templList = new List<string>();
+            using (StreamReader sr = File.OpenText(fileName))
+            {
+                string s = String.Empty;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    templList.Add(s.Split(separator).First().Trim());
+                }
+            }
+
+            return templList;
+        }
+
+        /// <summary>
+        /// загрузка данных с предварит выборкой по стране
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="contry"></param>
+        /// <returns></returns>
+        public static List<string[]> ReadGeoData(string fileName, string country = "BY")
+        {
+            char separator = '\t';
+            List<string[]> geoList = new List<string[]>();
+
+            var start = DateTime.Now;
+            using (FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    var geoLine = s.Split(separator);
+                    if (geoLine.Contains(country))
+                        geoList.Add(geoLine);
+                }
+            }
+            var end = DateTime.Now;
+            //Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            //Console.WriteLine("Time: " + (end - start));
+            //Console.WriteLine();
+
+            GC.Collect();
+
+            return geoList;
+        }
+
+        /// <summary>
+        /// сопоставить данные и шаблон
+        /// </summary>
+        /// <param name="geoData"></param>
+        /// <param name="geoTemplate"></param>
+        /// <returns></returns>
+        internal static List<Dictionary<string, string>> Collate(List<string[]> geoData, List<string> geoTemplate)
+        {
+            if (geoData == null || geoTemplate == null || geoData.Count == 0 || geoTemplate.Count == 0)
+                throw new MissingFieldException("Отсутствуют данные или шаблон данных.");
+
+            if (geoData.First().Count() != geoTemplate.Count)
+                throw new MissingFieldException("Данные не соотв шаблону.");
+
+            List<Dictionary<string, string>> geoMapList = new List<Dictionary<string, string>>();
+
+            foreach (var geo in geoData)
+            {
+                var dic = geoTemplate.Zip(geo, (k, v) => new { key = k, value = v }).ToDictionary(x => x.key, x => x.value);
+                geoMapList.Add(dic);
+            }
+
+            return geoMapList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFLabsGeonames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // http://download.geonames.org/
+            // топ 50 городов Беларус по населению
+            // ID geonamesId, Название кириллица, название латиница, индексЮ, широта, долгота, население  (CSV, utf-8)
+
+
+            DateTime end;
+            DateTime start = DateTime.Now;
+            Console.WriteLine("### Start Time: " + start.ToLongTimeString());
+            Console.WriteLine();
+
+
+            var geoTemplate = Geoname.ReadTemplate("Template/geonames_template.txt");
+            var geoPostCodeTemplate = Geoname.ReadTemplate("Template/geonames_postal_template.txt");
+
+            var geoData = Geoname.ReadGeoData("Data/BY_data.txt");
+            var geoPostCodeData = Geoname.ReadGeoData("Data/BY_postcode.txt");
+            //var geoData = Geoname.ReadGeoData("D://work//Data//ALL_data.txt", "BY");
+            //var geoPostCodeData = Geoname.ReadGeoData("D://work//Data//ALL_postcode.txt", "BY");
+
+            Console.WriteLine("### Geo data lines: " + geoData.Count);
+            Console.WriteLine("### GeoPost data lines: " + geoPostCodeData.Count);
+
+            var geoDataMap = Geoname.Collate(geoData, geoTemplate);
+            var geoPostMap = Geoname.Collate(geoPostCodeData, geoPostCodeTemplate);
+
+            // PPL	populated place	a city, town, village, or other agglomeration of buildings where people live and work (http://www.geonames.org/export/codes.html)
+            geoDataMap = geoDataMap.Where(g => !g["population"].Equals("0") && g["country code"].Equals("BY") && g["feature class"].Equals("P") /*g["feature code"].Equals("PPL")*/) // PPL2-4 надо? / А-область
+                .ToList(); //<Dictionary<string, string>>
+
+            geoPostMap = geoPostMap.Where(p => p["country code"].Equals("BY")).ToList();
+
+            // связываем по координатам - приходится округлять, т.к. разные знач - после 2 знака после зпт
+            var geoBy50 = (from itemA in geoDataMap
+                           from itemB in geoPostMap
+                           where (
+                           Math.Round(double.Parse(itemA["latitude"].Replace(".", ",")), 2) == Math.Round(double.Parse(itemB["latitude"].Replace(".", ",")), 2)
+                           && Math.Round(double.Parse(itemA["longitude"].Replace(".", ",")), 2) == Math.Round(double.Parse(itemB["longitude"].Replace(".", ",")), 2)
+                           && !string.IsNullOrEmpty(itemB["admin name2"])
+                           )
+                           select new
+                           {
+                               geonameId = itemA["geonameid"],
+                               cyrilName = itemB["place name"],
+                               latinName = itemA["name"],
+                               postCode = itemB["postal code"],
+                               _asciiname = itemA["asciiname"],
+                               _admname2 = itemB["admin name2"],
+                               latitude = itemA["latitude"],
+                               longitude = itemA["longitude"],
+                               _latitude = Math.Round(double.Parse(itemB["latitude"].Replace(".", ",")), 2),
+                               _longitude = Math.Round(double.Parse(itemB["longitude"].Replace(".", ",")), 2),
+                               population = itemA["population"],
+
+                               _AB = itemA["asciiname"].Except(itemB["admin name2"]).ToList().Aggregate(new StringBuilder(), (i, j) => i.Append(j)),
+                               _BA = itemB["admin name2"].Except(itemA["asciiname"]).ToList()
+
+
+                           }
+                        ).OrderByDescending(i => int.Parse(i.population)).Take(50).ToList();
+
+            //save
+            //using (StreamWriter sw = File.CreateText("BY_out.csv"))
+            using (StreamWriter sw = new StreamWriter(File.Open("BY_out.csv", FileMode.Create), Encoding.UTF8))
+            {
+                foreach (var g in geoBy50)
+                {
+                    sw.WriteLine($"{g.geonameId}\t{g.cyrilName}\t{g.latinName}\t{g.postCode}\t{g.latitude}\t{g.longitude}\t{g.population}");
+                }
+            }
+
+            end = DateTime.Now;
+            Console.WriteLine();
+            Console.WriteLine("### End Time: " + end.ToLongTimeString());
+            Console.WriteLine("### Run Time: " + (end - start));
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
